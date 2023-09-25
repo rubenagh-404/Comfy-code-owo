c26b6d471e60b8ab9b8731bed8929eb425cafaaf
rubenagh-404 <141638053+rubenagh-404@users.noreply.github.com>
2023-09-25 12:24:16 +0200

    avslutning emne 1

--- Emne 1/Oppgaver/Oppgave 128 G/script.js
+++ Emne 1/Oppgaver/Oppgave 128 G/script.js
@@ -63,3 +63,37 @@ function drawBoard() {
     html += `</div>`;
     return html;
 }
+
+// løsningsforslag
+// // model
+// let pieces = [
+//     '♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
+//     '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
+//     '', '', '', '', '', '', '', '',
+//     '', '', '', '', '', '', '', '',
+//     '', '', '', '', '', '', '', '',
+//     '', '', '', '', '', '', '', '',
+//     '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
+//     '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖',
+// ];
+
+// // view
+// updateView()
+// function updateView() {
+//     document.getElementById('app').innerHTML = drawBoard();
+// }
+
+// function drawBoard() {
+//     let html = `<div id="board">`;
+//     for (let i = 0; i < pieces.length; i++) {
+//         const col = i % 8;
+//         const row = Math.floor(i / 8);
+//         const isEvenCol = col % 2 == 0;
+//         const isEvenRow = row % 2 == 0;
+//         const isDark = isEvenRow && !isEvenCol || !isEvenRow && isEvenCol;
+//         const color = isDark ? 'dark' : 'light';
+//         html += `<div class="${color}Square">${pieces[i]}</div>`;
+//     }
+//     html += `</div>`;
+//     return html;
+// }
--- Emne 1/løkker/151A/script.js
+++ Emne 1/løkker/151A/script.js
@@ -1,35 +1,36 @@
 // model
-
+let model = {
+    text: "Skriv noe her :3 ",
+    maxLength: 20,
+};
+let firstInput = false;
 
 // view
 updateView()
 function updateView() {
     let html = '';
-    html += `<input type="text" id="tekst" value="skriv noe her :3">`
-    html += `<input type="number" id="tekst" value="">`
-    html += `<br><button>doble tekst</button>`
-    
+    html += `<input type="text" id="userText" oninput="model.text = this.value" value="${model.text}">`;
+    html += `<input type="number" id="userNumber" oninput="model.maxLength = this.value" value="${model.maxLength}">`;
+    html += `<br><button onclick="printText()">Doble tekst</button>`;
+    if (firstInput) {
+    html += `<p id="text">${model.text}</p>`
+    }
     document.getElementById('app').innerHTML = html;
 }
 
 
 // controller
+function printText() {
+    firstInput = true;
+    if (model.text.length < model.maxLength) {
+        model.text = model.text += model.text;
+    }
+    for (let i = 0; i < cars.length; i++) {
+        text += cars[i] + "<br>";
+      }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    updateView()
+}
 
 // Lag et tekst-input-felt "tekst" og et tall-input-felt "maks. lengde".
 // Lag en knapp "gjenta". Når brukeren trykker på denne knappen,
